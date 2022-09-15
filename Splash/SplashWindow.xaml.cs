64714f6b269b23eca6f6297ed687acc6af3e77f3
Roman Permiakov <info@presstomsk.ru>
2022-09-15 18:38:50 +0700

    SplashWindow

---
+++ Splash/SplashWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+
+namespace Splash.View
+{   
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }        
+    }
+}
